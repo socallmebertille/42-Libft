cbedeb5412fc95636e6f0c73c0100933b44db966
Sarah <163013013+socallmebertille@users.noreply.github.com>
2024-05-17 16:08:45 +0000

    changes on ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -13,6 +13,17 @@
 int ft_isalpha(int c)
 {
     if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-        return (0);
-    return (1);
+        return (1);
+    return (0);
 }
+
+/*#include <stdio.h>
+
+int main(void)
+{
+    int c;
+
+    c = 97;
+    printf("%d\n", ft_isalpha(c));
+    return (0);
+}*/
