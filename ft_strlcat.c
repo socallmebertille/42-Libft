09d05a43f46b25ce2811bf3382c6651458554376
Sarah Berton <saberton@bess-f4r3s13.clusters.42paris.fr>
2024-05-23 14:15:49 +0200

    changes on ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: saberton <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 18:09:29 by saberton          #+#    #+#             */
-/*   Updated: 2024/05/23 12:21:38 by saberton         ###   ########.fr       */
+/*   Updated: 2024/05/23 14:15:29 by saberton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t siz)
 {
-	unsigned int	i;
-	unsigned int	j;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
+	if (siz == 0)
+		return (ft_strlen(src));
 	while (dst[i] && i < siz - 1)
 		i++;
 	j = 0;
-	while (i < siz - 1)
+	while (src[j] && i < siz - 1)
 		dst[i++] = src[j++];
-	if (src[j])
+	if (!src[j + 1])
 		dst[i] = src[j];
 	else
 		dst[i] = '\0';
@@ -32,17 +34,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t siz)
 	return (i);
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 #include <string.h>
 
 int     main(void)
 {
-	char    dest[15] = "Hola que tal ?";
-        char    src[11] = "Muy bien !";
+	char    dest[6] = "Hello";
+        char    *src = " Wolrd";
+	size_t	dstsiz = sizeof(dest);
 
 	printf("Chaine dest : %s\n", dest);
         printf("Chaine src : %s\n", src);
-        printf("La chaine concatene dest devrait avoir %zu caractere(s).\n",ft_strlcat(dest, src, 24));
-        printf("Voici a quoi ressemble maintenant dest : %s", dest);
+        printf("Taille ideale de dest : %zu\n",ft_strlcat(dest, src, dstsiz));
+        printf("Chaine dest apres ft_strlcat: %s\n", dest);
         return (0);
-}
+}*/
