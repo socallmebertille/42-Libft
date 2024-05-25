292c3d7aefea3725e434ac573f6f78fcbd44a432
Sarah Berton <saberton@bess-f2r3s15.clusters.42paris.fr>
2024-05-25 15:06:23 +0200

    added ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,65 +6,70 @@
 /*   By: saberton <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 21:07:39 by saberton          #+#    #+#             */
-/*   Updated: 2024/05/24 22:09:57 by saberton         ###   ########.fr       */
+/*   Updated: 2024/05/25 15:06:08 by saberton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_count(int n)
+int	ft_count(long nb)
 {
 	int	count;
 
 	count = 0;
-	if (n <  0)
-		count++;
-	while (n > 9)
+	if (nb < 0)
 	{
-		n /= 10;
+		count++;
+		nb = -nb;
+	}
+	while (nb > 9)
+	{
+		nb = nb / 10;
 		count++;
 	}
+	count++;
 	return (count);
 }
 
 char	*ft_itoa(int n)
 {
 	int 	count;
+	long	nb;
 	char	*s;
 
-	count = ft_count(n);
-	if (n < 0)
-		count += 1;
+	nb = n;
+	count = ft_count(nb);
 	if (!count)
 		return (NULL);
-	s = (char *)malloc(sizeof(char) * (count + 1));
+	s = (char *)malloc(sizeof(char) * (count + 2));
 	if (!s)
 		return (NULL);
-	while (count-- > 0)
+	printf("%d\n", count + 2);
+	if (nb < 0)
 	{
-		if (n < 0)
-		{
-			n = -n;
-			*s++ = "-";
-		}
-		while (n > 9)
-		{
-			
-		}
-		
+		nb = -nb;
+		s[0] = '-';
 	}
+	s[count--] = '\0';
+	while (nb > 9)
+	{
+		s[count--] = nb % 10 + '0';
+		nb = nb / 10;
+	}
+	s[count--] = nb + '0';
+	return (s);
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 
-int	main(int ac, char **av)
+int	main(void)
 {
 	char	*s;
+	int	n = INT_MIN;
 
-	if (ac > 2)
-		return (1);
-	printf("int d'origine : %d\n", av[1]);
-	s = ft_itoa(av[1]);
-	printf("chaine : %s\n", s)
+	printf("int d'origine : %d\n", n);
+	s = ft_itoa(n);
+	printf("chaine : %s\n", s);
+	free(s);
 	return (0);
-}
+}*/
